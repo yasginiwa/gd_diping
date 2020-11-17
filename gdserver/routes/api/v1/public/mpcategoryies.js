ee0333fe267d6be6cb30cb70a3944def86bc44a1
yasginiwa <23801504@qq.com>
2020-11-17 20:51:09 +0800

    first commit

---
+++ gdserver/routes/api/v1/public/mpcategoryies.js
@@ -0,0 +1,8 @@
+const router = require('koa-router')()
+const dao = require('../../../../modules/dao')
+
+router.get('/', async(ctx, next) => {
+    
+})
+
+module.exports = router.routes()
---
+++ gdserver/routes/api/v1/public/mphome.js
@@ -0,0 +1,2 @@
+const router = require('koa-router')()
+const dao = require('../../../../modules/dao')
