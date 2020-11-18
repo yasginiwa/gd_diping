be987f3cb940beb065c5d57e5142d36f42925b20
李玉刚 <liyugang@liyugangs-Mac-mini.local>
2020-11-18 17:19:08 +0800

    开始分类界面编写

--- gdserver/db/gddb.sql
+++ gdserver/db/gddb.sql
@@ -25,10 +25,10 @@ DROP TABLE IF EXISTS `t_categories`;
 CREATE TABLE `t_categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) DEFAULT NULL,
-  `pid` int(11) DEFAULT NULL,
-  `p_level` tinyint(4) DEFAULT NULL,
+  `cate_level` tinyint(4) DEFAULT NULL,
+  `parent_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,65 @@ CREATE TABLE `t_categories` (
 
 LOCK TABLES `t_categories` WRITE;
 /*!40000 ALTER TABLE `t_categories` DISABLE KEYS */;
+INSERT INTO `t_categories` VALUES (1,'地坪设备',1,0),(2,'金钢工具',1,0),(3,'配件工具',1,0),(4,'机械配件',1,0),(5,'五金工具',1,0),(6,'清洁工具',1,0),(7,'电工工具',1,0),(8,'地坪工具耗材',1,0),(9,'研磨机',2,1),(10,'打磨机',2,1),(11,'抹光机',2,1),(12,'铲削机',2,1),(13,'抛光机',2,1),(14,'翻新机',2,1),(15,'洗地机',2,1),(16,'吸水机',2,1),(17,'吸尘器',2,1),(18,'铣刨机',2,1),(19,'升降机',2,1),(20,'一体机',2,1);
 /*!40000 ALTER TABLE `t_categories` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_comments`
+--
+
+DROP TABLE IF EXISTS `t_comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `custom_avatar` varchar(255) DEFAULT NULL,
+  `custom_name` varchar(255) DEFAULT NULL,
+  `rate` tinyint(4) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_comments`
+--
+
+LOCK TABLES `t_comments` WRITE;
+/*!40000 ALTER TABLE `t_comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_products`
+--
+
+DROP TABLE IF EXISTS `t_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `t_products` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `category` int(11) DEFAULT NULL,
+  `price` decimal(7,2) DEFAULT NULL,
+  `parameters` varchar(255) DEFAULT NULL,
+  `features` varchar(255) DEFAULT NULL,
+  `introduce` varchar(255) DEFAULT NULL,
+  `sold_count` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_products`
+--
+
+LOCK TABLES `t_products` WRITE;
+/*!40000 ALTER TABLE `t_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_products` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -78,4 +136,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-11-17 17:09:29
+-- Dump completed on 2020-11-18 17:17:57
--- gdserver/routes/api/v1/public/mp/mpcategoryies.js
+++ gdserver/routes/api/v1/public/mp/mpcategoryies.js
@@ -1,5 +1,5 @@
 const router = require('koa-router')()
-const dao = require('../../../../modules/dao')
+const dao = require('../../../../../modules/dao')
 
 router.get('/', async(ctx, next) => {
     
