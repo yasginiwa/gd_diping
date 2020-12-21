9b6ae858c9e6086171b9edd69209bb43bcb1f5f2
yasginiwa <23801504@qq.com>
2020-12-21 21:03:48 +0800

    生成海报待完成

--- gdmp/pages/detail/detail.js
+++ gdmp/pages/detail/detail.js
@@ -1,6 +1,159 @@
 // pages/detail/detail.js
 import { request } from '../../utils/request'
 import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast'
+import Poster from '../../miniprogram_npm/wxa-plugin-canvas/poster/poster'
+
+
+const posterConfig = {
+  jdConfig: {
+    width: 750,
+    height: 1334,
+    backgroundColor: '#fff',
+    debug: false,
+    pixelRatio: 1,
+    blocks: [
+      {
+        width: 690,
+        height: 808,
+        x: 30,
+        y: 183,
+        borderWidth: 2,
+        borderColor: '#f0c2a0',
+        borderRadius: 20,
+      },
+      {
+        width: 634,
+        height: 74,
+        x: 59,
+        y: 770,
+        backgroundColor: '#fff',
+        opacity: 0.5,
+        zIndex: 100,
+      },
+    ],
+    texts: [
+      {
+        x: 113,
+        y: 61,
+        baseLine: 'middle',
+        text: '伟仔',
+        fontSize: 32,
+        color: '#8d8d8d',
+      },
+      {
+        x: 30,
+        y: 113,
+        baseLine: 'top',
+        text: '发现一个好物，推荐给你呀',
+        fontSize: 38,
+        color: '#080808',
+      },
+      {
+        x: 92,
+        y: 810,
+        fontSize: 38,
+        baseLine: 'middle',
+        text: '标题标题标题标题标题标题标题标题标题',
+        width: 570,
+        lineNum: 1,
+        color: '#8d8d8d',
+        zIndex: 200,
+      },
+      {
+        x: 59,
+        y: 895,
+        baseLine: 'middle',
+        text: [
+          {
+            text: '2人拼',
+            fontSize: 28,
+            color: '#ec1731',
+          },
+          {
+            text: '¥99',
+            fontSize: 36,
+            color: '#ec1731',
+            marginLeft: 30,
+          }
+        ]
+      },
+      {
+        x: 522,
+        y: 895,
+        baseLine: 'middle',
+        text: '已拼2件',
+        fontSize: 28,
+        color: '#929292',
+      },
+      {
+        x: 59,
+        y: 945,
+        baseLine: 'middle',
+        text: [
+          {
+            text: '商家发货&售后',
+            fontSize: 28,
+            color: '#929292',
+          },
+          {
+            text: '七天退货',
+            fontSize: 28,
+            color: '#929292',
+            marginLeft: 50,
+          },
+          {
+            text: '运费险',
+            fontSize: 28,
+            color: '#929292',
+            marginLeft: 50,
+          },
+        ]
+      },
+      {
+        x: 360,
+        y: 1065,
+        baseLine: 'top',
+        text: '长按识别小程序码',
+        fontSize: 38,
+        color: '#080808',
+      },
+      {
+        x: 360,
+        y: 1123,
+        baseLine: 'top',
+        text: '超值好货一起拼',
+        fontSize: 28,
+        color: '#929292',
+      },
+    ],
+    images: [
+      {
+        width: 62,
+        height: 62,
+        x: 30,
+        y: 30,
+        borderRadius: 62,
+        url: 'http://172.16.1.227:3000/uploads/112_1.png',
+      },
+      {
+        width: 634,
+        height: 634,
+        x: 59,
+        y: 210,
+        url: 'http://172.16.1.227:3000/uploads/112_1.png',
+      },
+      {
+        width: 220,
+        height: 220,
+        x: 92,
+        y: 1020,
+        url: 'http://172.16.1.227:3000/uploads/112_1.png',
+      }
+    ]
+
+  }
+}
+
 
 Page({
 
@@ -17,7 +170,10 @@ Page({
     introduceActive: 0,
     typeIdx: -1,
     cart: [],
-    cartBadgeNum: ''
+    cartBadgeNum: '',
+    posterConfig: posterConfig.jdConfig,
+    createPosterShow: false,
+    posterImg: ''
   },
 
   /**
@@ -45,7 +201,7 @@ Page({
     //  设置底部栏购物车图标badge
     let cartBadgeNum = !wx.getStorageSync('cartBadgeNum') ? 0 : parseInt(wx.getStorageSync('cartBadgeNum'))
 
-    if (cartBadgeNum === 0) cartBadgeNum = '' 
+    if (cartBadgeNum === 0) cartBadgeNum = ''
 
     this.setData({ product, totalSoldCount, price, cartBadgeNum })
 
@@ -218,7 +374,7 @@ Page({
    * @param {*} delta 返回页数差值
    * @param {*} toPage 切换至的页面
    */
-  navBackAndSwithTab(delta, toTab){
+  navBackAndSwithTab(delta, toTab) {
     wx.navigateBack({
       delta: delta,
       complete: () => {
@@ -236,6 +392,9 @@ Page({
     this.handleShareClose()
   },
 
+ /**
+   * 处理分享给微信好友
+   */
   onShareAppMessage() {
     let { product } = this.data
     return {
@@ -246,12 +405,15 @@ Page({
   },
 
   /**
-   * 处理分享至朋友圈 仅支持安卓
+   * 处理分享至朋友圈 
    */
   handleShareToTimeline() {
     this.handleShareClose()
   },
 
+   /**
+   * 处理分享到朋友圈 仅支持安卓
+   */
   onShareTimeline() {
     let { product } = this.data
     return {
@@ -262,24 +424,42 @@ Page({
   },
 
   /**
-   * 处理生成海报分享到朋友圈
+   * 处理生成海报图片分享到朋友圈
    */
-  handleGeneratePop() {
+  handleGeneratePoster() {
+    this.setData({ posterConfig: posterConfig.jdConfig, createPosterShow: true }, () => {
+      Poster.create(true)    // 入参：true为抹掉重新生成
+    })
 
+    this.handleShareClose()
   },
 
   /**
-   * 处理分享到朋友圈
+   * 异步海报生成成功回调
    */
-  onShareTimeline() {
+  onPosterSuccess(e) {
+    const { detail } = e;
+    this.setData({ posterImg: detail })
+  },
 
+  /**
+   * 异步海报生成失败回调 
+   */
+  onPosterFail(err) {
+    console.error(err);
+  },
+
+ 
+
+  touchmove() {
+    // 重写此方法 因为是cover 所以禁用掉touchmove手势
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
